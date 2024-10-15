76e81b71de6ff1284abd6c44a2e63154cc5c086a
jerrysanguyo <jsanguyo1624@gmail.com>
2024-10-15 14:09:38 +0800

    add controller Not done

---
+++ app/Http/Controllers/PhysicalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePhysicalRequest;
+use App\Http\Requests\UpdatePhysicalRequest;
+use App\Models\Physical;
+
+class PhysicalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePhysicalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Physical $physical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Physical $physical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePhysicalRequest $request, Physical $physical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Physical $physical)
+    {
+        //
+    }
+}
